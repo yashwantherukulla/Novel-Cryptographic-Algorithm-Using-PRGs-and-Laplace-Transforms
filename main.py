56f8e27b3026fa15d25433d5cf20999068fb9a5e
yashwantherukulla <erukullayashwanth2005@gmail.com>
2024-06-19 16:50:39 +0530

    completed till shuffling part of encryption

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import secrets
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+import random
+from math import factorial
 
 class NCA:
     def __init__(self, file_path):
         self.file_path = file_path
         self.plain_txt = self.__get_plain_text()
         self.PRS = self.__gen_PRS(self.__get_seq_len())
-        self.ops = self.__get_ops(len(self.plain_txt))
+        self.ops = self.__get_ops()
     
     def __gen_TRN(self):
         rb_size = secrets.token_bytes(1)
@@ -40,9 +42,9 @@ class NCA:
             
             return ''.join(prns)[:seq_len]
 
-    def __get_plain_txt_len(self):
-        with open(self.file_path, 'r') as file:
-            return len(file.read())
+    # def __get_plain_txt_len(self):
+    #     with open(self.file_path, 'r') as file:
+    #         return len(file.read())
     
     def __get_plain_text(self):
         with open(self.file_path, 'r') as file:
@@ -52,8 +54,9 @@ class NCA:
         with open(self.file_path, 'r') as file:
             return 2*(len(file.read())*3) + len(file.read()) + 7
         
-    def __get_ops(self, plain_txt_len: int):
+    def __get_ops(self):
         ops = []
+        plain_txt_len = len(self.plain_txt)
         ops.append(int(self.PRS[:7]) % 7)
         for i in range(1, plain_txt_len):
             ops.append(281*ops[i-1] + 443)
@@ -62,10 +65,25 @@ class NCA:
 
     def __get_encoded_wo_shuffle(self):
         enc_wo_s = []
+        skipped_chunks = 0
         for i in range(len(self.plain_txt)):
-            ascii_val = int(self.plain_txt[i])
             chunk = int(self.PRS[7+i] + self.PRS[7+i+1] + self.PRS[7+i+2]) # 7 goes for the op
-            enc_wo_s.append(ascii_val + (chunk))
+            if chunk!=0:
+                ascii_val = ord(self.plain_txt[i])
+                enc_wo_s.append(ascii_val + (chunk * self.ops[i]))
+            else:
+                skipped_chunks += 1
+        
+        return enc_wo_s, skipped_chunks
+    
+    def __get_shuffled(self, enc_wo_s: list, skipped_chunks: int):
+        remaining_digits = self.PRS[:(7+len(self.plain_txt)+skipped_chunks)]
+        shuffle_seed = int(remaining_digits) % factorial(len(self.plain_txt))
+        
+        random.seed(shuffle_seed)
+        random.shuffle(enc_wo_s)
+        enc_w_s = enc_wo_s
+        return enc_w_s
 
 
     def encrypt(self):
@@ -75,12 +93,16 @@ class NCA:
 
 if __name__ == '__main__':
     nca = NCA('plain.txt')
-    plain_txt_len = nca._NCA__get_plain_txt_len()
-    print(plain_txt_len)
+    # plain_txt_len = nca._NCA__get_plain_txt_len()
+    # print(plain_txt_len)
     seq_len = nca._NCA__get_seq_len()
     print(seq_len)
     print("-------------------")
     nca._NCA__gen_PRS(seq_len)
     print(nca.PRS[:7])
-    ops = nca._NCA__get_ops(plain_txt_len)
+    ops = nca._NCA__get_ops()
     print(ops)
+    half_enc, skips = nca._NCA__get_encoded_wo_shuffle()
+    print(half_enc, skips)
+    shuffled = nca._NCA__get_shuffled(half_enc, skips)
+    print(shuffled)
